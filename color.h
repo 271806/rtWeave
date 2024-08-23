02b5dc3a4637f3889c02bcd745fa45b35ee2a3fe
yinfeiqian <bobq1998@gmail.com>
2024-08-23 21:38:47 +0900

    new feature: anti-aliasing with multiple samples

--- color.h
+++ color.h
@@ -1,6 +1,7 @@
 #ifndef CLOROR_H
 #define COLOR_H
 
+#include "interval.h"
 #include "vec3.h"
 #include "rtweekend.h"
 
@@ -15,9 +16,11 @@ void write_color(std::ostream& out, const color& pixel_color) {
 
     // scale [0, 1] to [0, 255]
     // multiply by 255.999 to accurately scale the values
-    int rbyte = int(255.999 * r);
-    int gbyte = int(255.999 * g);
-    int bbyte = int(255.999 * b);
+    // * Translate the [0,1] component values to the byte range [0,255].
+    static const interval intensity(0.000, 0.999);
+    int rbyte = int(256 * intensity.clamp(r));
+    int gbyte = int(256 * intensity.clamp(g));
+    int bbyte = int(256 * intensity.clamp(b));
 
     // output the color values to the output stream (usually a file or standard output)
     out << rbyte << " " << gbyte << " " << bbyte << "\n";
